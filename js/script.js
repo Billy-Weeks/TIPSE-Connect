87d7538cdbe7477ddceb5c8a72f4aabf3c4a97b4
Billy Weeks <meznull43628@gmail.com>
2026-07-10 22:22:53 -0700

    Added javascript logic into separate file

---
+++ js/script.js
@@ -0,0 +1,56 @@
+// Variable to store form
+const elementForm = document.getElementById('connect-form');
+
+// Add even listener to elementForm
+elementForm.addEventListener('submit', function (event) {
+    // Anonymous function (??) to prevent default submission behavior
+    event.preventDefault();
+
+    // Grab values from form inputs
+    const formFirst = document.getElementById('first').value;
+    const formLast = document.getElementById('last').value;
+    const cinForm = document.getElementById('cin').value;
+    const emailForm = document.getElementById('email').value;
+    const experienceForm = document.getElementById('experience').value;
+    const majorForm = document.getElementById('major').value;
+
+    // test prints
+    console.log('First Name: ' + formFirst);
+    console.log('Last Name: ' + formLast);
+    console.log('CIN: ' + cinForm);
+    console.log('Email: ' + emailForm);
+    console.log('Experience Level: ' + experienceForm);
+    console.log('Major: ' + majorForm);
+
+    // Generate session ID for use in rate limiter (security measure)
+    const currentSessionId = crypto.randomUUID();
+
+    // Fetch request to server with form data
+    fetch("https://jyzcfhadrqpdhjlunjav.supabase.co/rest/v1/members", {
+        method: "POST",
+        headers: {
+            "apikey": "sb_publishable_JMMPbDQIpBhZWIZQXaclCQ_iTUbm_1W",
+            "Authorization": "Bearer sb_publishable_JMMPbDQIpBhZWIZQXaclCQ_iTUbm_1W",
+            "Content-Type": "application/json",
+            "x-form-session-id": currentSessionId
+
+        },
+        body: JSON.stringify({
+            first_name: formFirst,
+            last_name: formLast,
+            cin: cinForm,
+            email: emailForm,
+            experience: experienceForm,
+            major: majorForm
+        })
+    })
+        .then(response => {
+            if (!response.ok) {
+                console.error("Database rejected the request!");
+            }
+            else {
+                console.log("Successfully sent to Supbase!");
+            }
+            
+        })
+});
